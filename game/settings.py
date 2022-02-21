ba1181b1e48390541e21004268dfc09217ed7e95
sifana-va <sifanava@gmail.com>
2022-02-21 15:30:08 -0800

    changed allowhost

--- game/settings.py
+++ game/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-62umjimfl4)oq(q!69_e48bat#w76^5ccla=7)gqyio2$9h3gk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
