61dddd104dd540146f1120bc4378d2ea5b51331c
sifana-va <sifanava@gmail.com>
2022-02-21 15:18:04 -0800

    commiting new project

---
+++ user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request,"home.html")
+
+def document(doc):
+    return render(doc,"work.html")
+    
